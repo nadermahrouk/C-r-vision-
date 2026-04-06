723bd58f0abc5a48863be3e5309a086f88f39747
Nader Mahrouk <72823537+nadermahrouk@users.noreply.github.com>
2026-04-06 19:09:40 +0100

    Ajout exemple constante.cpp

---
+++ constante.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std ; 
+
+int main(){
+    double const pi {3.141592};
+    cout << pi << endl ;  
+    return 0 ;
+
+}
+
+// alors une constante tu ne peux plus changer sa valeur 
+/*
+Ils sont identiques : 
+const double pi{3.14};
+double const pi{3.14}; */
+
+
+
