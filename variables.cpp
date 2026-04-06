3b46d1be32d7b7d54a2dc049a06630a512284682
Nader Mahrouk <72823537+nadermahrouk@users.noreply.github.com>
2026-04-06 18:41:15 +0100

    Ajout de variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string> // cette bibilothéque ce faite car le c++ ne connait pas châine de caractére donc un expert qu'il a fait pour manipuler 
+
+using namespace std;
+
+int main() {
+    int variable = 42; 
+    cout << variable << endl ; 
+}
+
+
