7093e5db4dbfe55e3000a746c7bc9f3a3f3cae7b
Nader Mahrouk <72823537+nadermahrouk@users.noreply.github.com>
2026-04-06 19:47:56 +0100

    ajout de bool.cpp

---
+++ bool.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std ; 
+
+int main (){
+
+    bool const vrai = true ; 
+    bool const faux = false ; 
+    return 0;
+}
+
+/*
+
+Opérateur Signification Exemple
+== Égalité, compare si deux variables sont égales entre elles. a == b
+60
+II. Le début du voyage
+!= Inégalité, compare si deux variables sont de valeurs différentes. a != b
+< Strictement inférieur, compare si la variable de gauche est strictement inférieure à celle de droite.
+a < b
+<= Inférieur ou égale, compare si la variable de gauche est inférieure
+ou égale à celle de droite.
+a <= b
+> Strictement supérieur, compare si la variable de gauche est
+strictement supérieure à celle de droite.
+a > b
+>= Supérieur ou égale, compare si la variable de gauche est supérieure ou égale à celle de droite.
+a >= b
+*/
+
+// boolalpha donnez vrai pour 1 et faux rempalce 0
